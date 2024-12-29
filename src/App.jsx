abe33c2157f87393623e51fac7ff6c0a8d270f8a
lohithpuvvala <lohithpuvvala@gmail.com>
2024-12-29 13:13:28 +0530

    Created Brand dev-lohith

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <h1 className="text-3xl font-bold underline">
-      Hello Lohith!
+      Hello I am Lohith this is from dev-lohith branch!
     </h1>
   )
 }
